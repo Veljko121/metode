7e680456dbf9e08657446dc6f5da5bdbf2c0e22a
Veljko Nikolic <nikolicveljko01@gmail.com>
2022-12-09 16:36:11 +0100

    Added ADAM

--- test3.py
+++ test3.py
@@ -11,7 +11,7 @@ def steepest_descent(gradf, x0, gamma=1e-1, epsilon=1e-2, max_steps=1000):
             break
     return x
     
-def steepest_descent_with_momentum(gradf, x0, gamma=1e-1, omega=1e-2, epsilon=1e-2, max_steps=1000):
+def steepest_descent_with_momentum(gradf, x0, gamma=1e-1, omega=1e-1, epsilon=1e-2, max_steps=1000):
     x = np.array(x0).reshape(len(x0), 1)
     v = np.zeros(shape=x.shape)
     for steps in range(max_steps):
@@ -22,12 +22,28 @@ def steepest_descent_with_momentum(gradf, x0, gamma=1e-1, omega=1e-2, epsilon=1e
             break
     return x
 
+def adam_v(gradf, x0, gamma=1e-1, omega1=1e-1, omega2=1e-1, epsilon1=1e-2, epsilon=1e-2, max_steps=1000):
+    x = np.array(x0).reshape(len(x0), 1)
+    v = np.zeros(shape=x.shape)
+    m = np.ones(shape=x.shape)
+    for steps in range(max_steps):
+        g = np.asarray(gradf(x))
+        m = omega1*m + (1 - omega1)*g
+        v = omega2*v + (1-omega2)*np.multiply(g,g)
+        hat_v = np.abs(v/(1 - omega2))
+        hat_m = m/(1 - omega1)
+        x = x - gamma * np.ones(shape = g.shape)/np.sqrt(hat_v + epsilon1)*hat_m
+        if np.linalg.norm(g) < epsilon:
+            break
+    return x, v, m
+
 def main():
     f = lambda x: x[0]**2 + x[1]**2
     gradf = lambda x: np.array([2*x[0], 2*x[1]])
 
     # x = steepest_descent(gradf, [1, 1])
-    x = steepest_descent_with_momentum(gradf, [1, 1])
+    # x = steepest_descent_with_momentum(gradf, [1, 1])
+    x, _, _ = adam_v(gradf, [1, 1])
 
     x1v = np.linspace(-3, 3, 1000)
     x2v = np.linspace(-3, 3, 1000)
